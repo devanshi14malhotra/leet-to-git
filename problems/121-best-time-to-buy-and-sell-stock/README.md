48e8eed179e99a975bbe1d3a7c6f7b2feaf73544
Devanshi Malhotra <malhotra.dm07@gmail.com>
2026-08-25 10:20:59 +0000

    LeetCode submission: problem 121 - Best Time to Buy and Sell Stock (submission 2119476218)

---
+++ problems/121-best-time-to-buy-and-sell-stock/README.md
@@ -0,0 +1,7 @@
+# 121. Best Time to Buy and Sell Stock
+
+**Difficulty:** Easy
+
+**Topics:** Array, Dynamic Programming
+
+[View on LeetCode](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
---
+++ problems/121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0; 
+        int r=1;
+        int maxp=0;
+        while (r<prices.size()){
+            if (prices[l]<prices[r]){
+                int prof = prices[r]-prices[l];
+                maxp=max(maxp,prof);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return maxp;
+    }
+};
