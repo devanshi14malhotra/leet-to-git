8a351e304d1f980fdbb85d14cbb9ea0f96c306c3
Devanshi Malhotra <malhotra.dm07@gmail.com>
2026-09-09 18:55:36 +0000

    LeetCode submission: problem 349 - Intersection of Two Arrays (submission 2136664665)

---
+++ problems/349-intersection-of-two-arrays/README.md
@@ -0,0 +1,7 @@
+# 349. Intersection of Two Arrays
+
+**Difficulty:** Easy
+
+**Topics:** Array, Hash Table, Two Pointers, Binary Search, Sorting
+
+[View on LeetCode](https://leetcode.com/problems/intersection-of-two-arrays/)
---
+++ problems/349-intersection-of-two-arrays/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> result;
+        for (int x:nums2){
+            if (set1.count(x)){
+                result.insert(x);
+            }
+        }
+        return vector<int> (result.begin(), result.end());
+    }
+};
